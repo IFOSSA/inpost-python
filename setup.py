f6cacc3db234b7c072148e8004db9401707d2e9c
loboda4450 <loboda4450@gmail.com>
2022-03-17 23:34:49 +0100

    structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='inpost-python',
+    packages=find_packages(),
+    version='0.0.1',
+    description='Python API written in python',
+    author='loboda4450, mrkazik99',
+    author_email='loboda4450@gmail.com, mrkazik99@gmail.com',
+
+    license='LGPL 2.1',
+)
