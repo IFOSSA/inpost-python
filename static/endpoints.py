4e0f4698116b2eeebf9616096d65bbb076a064a8
Mrkazik99 <mateuszkazmierczak99@gmail.com>
2022-03-26 00:08:49 +0100

    Added some endpoints

--- static/endpoints.py
+++ static/endpoints.py
@@ -1,1 +1,6 @@
 login: str = 'https://manager.paczkomaty.pl/v1/api/auth/login'
+
+# \/ Secured by JWT \/
+
+user_infos: str = 'https://manager.paczkomaty.pl/v1/api/auth/user'
+get_parcels: str = 'https://manager.paczkomaty.pl/v1/api/parcels'
