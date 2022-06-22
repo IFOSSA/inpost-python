70a05ddb2a94b81a2872d5307ece35ee2ecf0840
loboda4450 <loboda4450@gmail.com>
2022-06-23 00:34:13 +0200

    setup update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='inpost-python',
     packages=find_packages(),
     version='0.0.1',
-    description='Python API written in python',
+    description='InPost API written in python',
     author='loboda4450, mrkazik99',
     author_email='loboda4450@gmail.com, mrkazik99@gmail.com',
 
