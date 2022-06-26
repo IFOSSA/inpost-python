3a7e8a97d8b88e626c0963c3aa7ee4ce80889e8b
loboda4450 <loboda4450@gmail.com>
2022-06-26 20:45:30 +0200

    new endpoints

--- static/endpoints.py
+++ static/endpoints.py
@@ -1,5 +1,5 @@
 login: str = 'https://manager.paczkomaty.pl/v1/api/auth/login'
-
+logout: str = 'https://manager.paczkomaty.pl/v1/api/auth/logout'
 # \/ Secured by JWT \/
 
 user_infos: str = 'https://manager.paczkomaty.pl/v1/api/auth/user'
