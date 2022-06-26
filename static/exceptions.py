59b981199e9b174df9c92307da07bfc866245d60
loboda4450 <loboda4450@gmail.com>
2022-06-26 20:46:34 +0200

    new exceptions

--- static/exceptions.py
+++ static/exceptions.py
@@ -1,12 +1,31 @@
 # ----------------- Parcels ----------------- #
+from typing import Optional, Any
+
+
 class SomeParcelError(Exception):
     pass
 
 
 # ----------------- API ----------------- #
 class NotAuthenticatedError(Exception):
-    pass
+    def __init__(self, reason):
+        super().__init__(reason)
+        self.msg: str = Optional[str]
+        self.reason: Any = Optional[Any]
 
+    @property
+    def stack(self):
+        return self.reason
+
+class SomeAPIError(Exception):
+    def __init__(self, reason):
+        super().__init__(reason)
+        self.msg: str = Optional[str]
+        self.reason: Any = Optional[Any]
+
+    @property
+    def stack(self):
+        return self.reason
 
 # ----------------- Other ----------------- #
 class NotImplementedError(Exception):
