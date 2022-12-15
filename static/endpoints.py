0bd13fa5ff01fc13a751c9d5f2fcfff81596c06b
loboda4450 <loboda4450@gmail.com>
2022-12-15 21:03:51 +0100

    single parcel endpoint

--- static/endpoints.py
+++ static/endpoints.py
@@ -7,3 +7,4 @@ logout: str = 'https://api-inmobile-pl.easypack24.net/v1/logout'  # post
 
 refresh_token: str = 'https://api-inmobile-pl.easypack24.net/v1/authenticate'  # post
 parcels: str = 'https://api-inmobile-pl.easypack24.net/v3/parcels/tracked'  # get
+parcel: str = 'https://api-inmobile-pl.easypack24.net/v3/parcels/tracked/'  # get
