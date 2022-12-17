c386535e4e9424d0efc771007e672a181a4edfde
loboda4450 <loboda4450@gmail.com>
2022-12-17 22:09:26 +0100

    little cleanup

--- static/endpoints.py
+++ static/endpoints.py
@@ -1,10 +1,10 @@
 login: str = 'https://api-inmobile-pl.easypack24.net/v1/authenticate'
 send_sms_code: str = 'https://api-inmobile-pl.easypack24.net/v1/sendSMSCode/'  # get
 confirm_sms_code: str = 'https://api-inmobile-pl.easypack24.net/v1/confirmSMSCode'  # post
-logout: str = 'https://api-inmobile-pl.easypack24.net/v1/logout'  # post
 
 # \/ Secured by JWT \/
 
 refresh_token: str = 'https://api-inmobile-pl.easypack24.net/v1/authenticate'  # post
 parcels: str = 'https://api-inmobile-pl.easypack24.net/v3/parcels/tracked'  # get
 parcel: str = 'https://api-inmobile-pl.easypack24.net/v3/parcels/tracked/'  # get
+logout: str = 'https://api-inmobile-pl.easypack24.net/v1/logout'  # post
